eff557a8fd1c92abc5f45b74faf2c43639513703
Sid Bachtiar <sid.bachtiar@gmail.com>
2022-04-10 19:01:01 +1200

    Word finder search by length and wild card

---
+++ generate_indexes.php
@@ -0,0 +1,21 @@
+<?php 
+
+$words = json_decode(file_get_contents('./words_dictionary.json'), true);
+
+$by_length = [];
+foreach ($words as $w => $i) {
+    $len = strlen($w);
+    if ($len >= 4 && $len < 16) {
+        $by_length['length_'.$len][] = $w;
+    }
+    echo '.';
+}
+echo "\n";
+
+file_put_contents("./words_dictionary_indexed.json", json_encode($by_length, JSON_PRETTY_PRINT));
+
+foreach ($by_length as $len => $words) {
+    echo "$len : ".count($words)." words\n";
+}
+
+
